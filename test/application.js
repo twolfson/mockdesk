2e438e14af3ee3ad3a85f662038b08ea1285cd38
Todd Wolfson <todd@twolfson.com>
2016-03-09 17:01:19 -0600

    Completed more tests

--- test/application.js
+++ test/application.js
@@ -1,10 +1,20 @@
 // Load in dependencies
-// var expect = require('chai').expect;
-// var Application = require('../lib/application');
+var expect = require('chai').expect;
+var Application = require('../lib/js/application');
 
 // Start our tests
 describe('An application', function () {
   it('has layers', function () {
-    // It works!
+    // Create our app
+    var container = document.createElement('div');
+    document.body.appendChild(container);
+    var app = new Application(container);
+
+    // Assert our app
+    expect(app).to.have.property('layers');
+    expect(app.layers).to.deep.equal([]);
+
+    // Teardown app/container
+    document.body.removeChild(container);
   });
 });
