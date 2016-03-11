49fb5041fa4b20f7a6eb6835a5ea7189298e293c
Todd Wolfson <todd@twolfson.com>
2016-03-11 17:09:20 -0600

    Added missing tests

---
+++ test/element-palette-click.js
@@ -0,0 +1,27 @@
+// Load in dependencies
+var expect = require('chai').expect;
+var simulant = require('simulant');
+var appUtils = require('./utils/application');
+var Rectangle = require('../lib/js/elements/rectangle');
+
+// Start our tests
+describe('A click in the element palette', function () {
+  appUtils.init();
+  before(function clickRectangleElement () {
+    // Find and click the rectangle element in the palette
+    var rectEl = this.container.querySelector('[data-element=Rectangle]');
+    simulant.fire(rectEl, 'click');
+  });
+
+  it('adds an element to the page', function () {
+    expect(this.app.layers).to.have.length(1);
+    var el = this.app.layers[0];
+    expect(el).to.be.instanceof(Rectangle);
+  });
+
+  it('positions the element at our expected position', function () {
+    var rectEl = this.container.querySelector('#workspace').childNodes[0];
+    expect(rectEl.style.top).to.equal('100px');
+    expect(rectEl.style.left).to.equal('100px');
+  });
+});
