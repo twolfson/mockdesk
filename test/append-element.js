28fe25b7a8e62e385e2703969bcc1da1348a9495
Todd Wolfson <todd@twolfson.com>
2016-03-11 17:09:13 -0600

    Updated and completed tests

--- test/append-element.js
+++ test/append-element.js
@@ -7,7 +7,7 @@ var Rectangle = require('../lib/js/elements/rectangle');
 describe('An application appending an element', function () {
   appUtils.init();
   before(function appendElement () {
-    this.el = new Rectangle({x: 0, y: 0, width: 10, height: 10});
+    this.el = new Rectangle({width: 10, height: 10});
     this.app.appendElement(this.el);
   });
   after(function cleanup () {
@@ -17,5 +17,9 @@ describe('An application appending an element', function () {
   it('adds the element to the last layer', function () {
     expect(this.app.layers).to.have.length(1);
     expect(this.app.layers[0]).to.equal(this.el);
+  });
+
+  it('adds the element to the workspace', function () {
+    expect(this.container.querySelector('#workspace').childNodes).to.have.length(1);
   });
 });
