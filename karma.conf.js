4efe6362f07cb476d04fc8101553e185765b235e
Todd Wolfson <todd@twolfson.com>
2016-03-16 17:12:53 -0500

    Got our CSS serving finally

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      {pattern: 'lib/css/main.css', included: false, served: true, watched: true},
       'test/*.js'
     ],
 
