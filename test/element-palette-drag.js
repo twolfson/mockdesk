52176ababa37657862f333dafb57828651cfb0d8
Todd Wolfson <todd@twolfson.com>
2016-03-18 19:33:13 -0500

    Got one-offs fixed

--- test/element-palette-drag.js
+++ test/element-palette-drag.js
@@ -8,25 +8,27 @@ var Rectangle = require('../lib/js/elements/rectangle');
 describe('A drag in the element palette', function () {
   appUtils.init();
   before(function saveRectanglePosition () {
-    var rectEl = this.container.querySelector('[data-element=Rectangle]');
-    this.rectStartBounds = rectEl.getBoundingClientRect();
+    var svg = this.container.querySelector('[data-element=Rectangle] > svg');
+    this.svgStartBounds = svg.getBoundingClientRect();
   });
   before(function dragRectangleElement (done) {
     // Find and drag the rectangle element in the palette
     // TODO: Get accurate starting points
     var svgEl = this.container.querySelector('[data-element=Rectangle] > svg');
-    simulant.fire(svgEl, 'mousedown', {button: 0, clientX: 100, clientY: 100});
+    var svgLeft = this.svgStartBounds.left;
+    var svgTop = this.svgStartBounds.top;
+    simulant.fire(svgEl, 'mousedown', {button: 0, clientX: svgLeft + 5, clientY: svgTop + 5});
     setTimeout(function dragEl () {
-      simulant.fire(svgEl, 'mousemove', {clientX: 110, clientY: 115});
+      simulant.fire(svgEl, 'mousemove', {clientX: svgLeft + 305, clientY: svgTop + 205});
     }, 20);
     setTimeout(function dragReleaseEl () {
-      simulant.fire(svgEl, 'mouseup', {clientX: 120, clientY: 130});
+      simulant.fire(svgEl, 'mouseup', {clientX: svgLeft + 305, clientY: svgTop + 205});
       done();
     }, 40);
   });
   appUtils.capturePage('element-palette-drag.png');
   after(function cleanup () {
-    delete this.rectStartBounds;
+    delete this.svgStartBounds;
   });
 
   it('adds an element to the app', function () {
@@ -36,11 +38,11 @@ describe('A drag in the element palette', function () {
   });
 
   it.only('positions the element at our expected position', function () {
-    var rectEl = this.container.querySelector('#workspace').childNodes[0];
-    var rectEndBounds = rectEl.getBoundingClientRect();
-    console.log(this.rectStartBounds);
-    console.log(rectEndBounds);
-    expect(rectEndBounds.left - this.rectStartBounds.left).to.equal(20);
-    expect(rectEndBounds.top - this.rectStartBounds.top).to.equal(30);
+    var svg = this.container.querySelector('#workspace').childNodes[0];
+    var rectEndBounds = svg.getBoundingClientRect();
+    console.log(this.svgStartBounds.top, this.svgStartBounds.left);
+    console.log(rectEndBounds.top, rectEndBounds.left);
+    expect(rectEndBounds.left - this.svgStartBounds.left).to.equal(300);
+    expect(rectEndBounds.top - this.svgStartBounds.top).to.equal(200);
   });
 });
