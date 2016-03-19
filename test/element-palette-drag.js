9acb0bcc640427f40b6f73b5f81fdb8a9c2e6665
Todd Wolfson <todd@twolfson.com>
2016-03-18 19:37:17 -0500

    Cleaned up code to make it more reusable

--- test/element-palette-drag.js
+++ test/element-palette-drag.js
@@ -8,26 +8,27 @@ var Rectangle = require('../lib/js/elements/rectangle');
 describe('A drag in the element palette', function () {
   appUtils.init();
   before(function saveRectanglePosition () {
-    var svg = this.container.querySelector('[data-element=Rectangle] > svg');
-    this.svgStartBounds = svg.getBoundingClientRect();
+    this.svgEl = this.container.querySelector('[data-element=Rectangle] > svg');
+    this.svgStartBounds = this.svgEl.getBoundingClientRect();
   });
   before(function dragRectangleElement (done) {
-    // Find and drag the rectangle element in the palette
-    // TODO: Get accurate starting points
-    var svgEl = this.container.querySelector('[data-element=Rectangle] > svg');
+    // Start dragging our element
     var svgLeft = this.svgStartBounds.left;
     var svgTop = this.svgStartBounds.top;
-    simulant.fire(svgEl, 'mousedown', {button: 0, clientX: svgLeft + 5, clientY: svgTop + 5});
+    simulant.fire(this.svgEl, 'mousedown', {button: 0, clientX: svgLeft + 5, clientY: svgTop + 5});
+
+    // Then drag and release our element
     setTimeout(function dragEl () {
-      simulant.fire(svgEl, 'mousemove', {clientX: svgLeft + 305, clientY: svgTop + 205});
+      simulant.fire(this.svgEl, 'mousemove', {clientX: svgLeft + 305, clientY: svgTop + 205});
     }, 20);
     setTimeout(function dragReleaseEl () {
-      simulant.fire(svgEl, 'mouseup', {clientX: svgLeft + 305, clientY: svgTop + 205});
+      simulant.fire(this.svgEl, 'mouseup', {clientX: svgLeft + 305, clientY: svgTop + 205});
       done();
     }, 40);
   });
   appUtils.capturePage('element-palette-drag.png');
   after(function cleanup () {
+    delete this.svgEl;
     delete this.svgStartBounds;
   });
 
@@ -38,10 +39,8 @@ describe('A drag in the element palette', function () {
   });
 
   it.only('positions the element at our expected position', function () {
-    var svg = this.container.querySelector('#workspace').childNodes[0];
-    var rectEndBounds = svg.getBoundingClientRect();
-    console.log(this.svgStartBounds.top, this.svgStartBounds.left);
-    console.log(rectEndBounds.top, rectEndBounds.left);
+    var svgEl = this.container.querySelector('#workspace').childNodes[0];
+    var rectEndBounds = svgEl.getBoundingClientRect();
     expect(rectEndBounds.left - this.svgStartBounds.left).to.equal(300);
     expect(rectEndBounds.top - this.svgStartBounds.top).to.equal(200);
   });
