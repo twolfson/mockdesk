05b5b264d36e805ff253cb078cddd01f45005f22
Todd Wolfson <todd@twolfson.com>
2016-03-18 19:25:10 -0500

    Fixed up names and adjusting testing intent

--- test/element-palette-drag.js
+++ test/element-palette-drag.js
@@ -7,20 +7,27 @@ var Rectangle = require('../lib/js/elements/rectangle');
 // Start our tests
 describe('A drag in the element palette', function () {
   appUtils.init();
+  before(function saveRectanglePosition () {
+    var rectEl = this.container.querySelector('[data-element=Rectangle]');
+    this.rectStartBounds = rectEl.getBoundingClientRect();
+  });
   before(function dragRectangleElement (done) {
     // Find and drag the rectangle element in the palette
     // TODO: Get accurate starting points
-    var rectEl = this.container.querySelector('[data-element=Rectangle] > svg');
-    simulant.fire(rectEl, 'mousedown', {button: 0, clientX: 100, clientY: 100});
+    var svgEl = this.container.querySelector('[data-element=Rectangle] > svg');
+    simulant.fire(svgEl, 'mousedown', {button: 0, clientX: 100, clientY: 100});
     setTimeout(function dragEl () {
-      simulant.fire(rectEl, 'mousemove', {clientX: 110, clientY: 110});
+      simulant.fire(svgEl, 'mousemove', {clientX: 110, clientY: 115});
     }, 20);
     setTimeout(function dragReleaseEl () {
-      simulant.fire(rectEl, 'mouseup', {clientX: 120, clientY: 120});
+      simulant.fire(svgEl, 'mouseup', {clientX: 120, clientY: 130});
       done();
     }, 40);
   });
   appUtils.capturePage('element-palette-drag.png');
+  after(function cleanup () {
+    delete this.rectStartBounds;
+  });
 
   it('adds an element to the app', function () {
     expect(this.app.layers).to.have.length(1);
@@ -30,7 +37,10 @@ describe('A drag in the element palette', function () {
 
   it.only('positions the element at our expected position', function () {
     var rectEl = this.container.querySelector('#workspace').childNodes[0];
-    expect(rectEl.style.top).to.equal('100px');
-    expect(rectEl.style.left).to.equal('100px');
+    var rectEndBounds = rectEl.getBoundingClientRect();
+    console.log(this.rectStartBounds);
+    console.log(rectEndBounds);
+    expect(rectEndBounds.left - this.rectStartBounds.left).to.equal(20);
+    expect(rectEndBounds.top - this.rectStartBounds.top).to.equal(30);
   });
 });
