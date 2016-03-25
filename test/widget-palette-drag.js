1ceac4d9a371424a422316e47d575b2940582d77
Todd Wolfson <todd@twolfson.com>
2016-03-25 02:01:26 -0500

    Fixed up terminology

--- test/widget-palette-drag.js
+++ test/widget-palette-drag.js
@@ -2,43 +2,43 @@
 var expect = require('chai').expect;
 var simulant = require('simulant');
 var appUtils = require('./utils/application');
-var Rectangle = require('../lib/js/elements/rectangle');
+var Rectangle = require('../lib/js/widgets/rectangle');
 
 // Start our tests
-describe('A drag in the element palette', function () {
-  // TODO: Consider abstracting element bounds resolving
-  //  `appUtils.findEl('svgEl', '[data-element=Rectangle] > svg');` // Saves `this.svgEl`
+describe('A drag in the widget palette', function () {
+  // TODO: Consider abstracting widget bounds resolving
+  //  `appUtils.findEl('svgEl', '[data-widget=Rectangle] > svg');` // Saves `this.svgEl`
   //  `appUtils.saveBounds('svgStartBounds', 'svgEl');` // Saves bounds for `this.svgEl` under `svgStartBounds`
-  //  `appUtils.saveBoundsBySelector('svgStartBounds', '[data-element=Rectangle] > svg');`
+  //  `appUtils.saveBoundsBySelector('svgStartBounds', '[data-widget=Rectangle] > svg');`
   //     I don't like this so much, doesn't feel reusable. Maybe a function like:
   //  `appUtils.saveBoundsBySelector('svgStartBounds',
-  //     function () { return document.querySelector('[data-element=Rectangle] > svg'); });`
+  //     function () { return document.querySelector('[data-widget=Rectangle] > svg'); });`
 
-  // TODO: Consider abstracting element clicking
+  // TODO: Consider abstracting widget clicking
   //  `appUtils.click('svgEl', {x: 100, y: 200})` clicks at said location
   //  `appUtils.click('svgEl', {x: 100, y: 200, offsetByBounds: 'svgStartBounds'})`
   //     This also feels magical, maybe we should stick to `before's` for these one-offs...
   /*
-  appUtils.findEl('svgEl', '[data-element=Rectangle] > svg');`
+  appUtils.findEl('svgEl', '[data-widget=Rectangle] > svg');`
   appUtils.saveBounds('svgStartBounds', 'svgEl');
-  before(function dragRectangleElement () {
+  before(function dragRectangleWidget () {
     // Dislike how both `click` and `findEl` look like they share the same syntax
     appUtils.click(this.svgEl, {x: 100, y: 200, offsetByBounds: this.svgStartBounds});
   });
   */
   // Maybe we define a `.memo()` syntax for saving to this? to make it more obvious
   /*
-  appUtils.memo('svgEl').saveEl('[data-element=Rectangle] > svg');`
+  appUtils.memo('svgEl').saveEl('[data-widget=Rectangle] > svg');`
   // This string is still annoying
   appUtils.memo(''svgStartBounds').saveBounds(appUtils.memo('svgEl'));
   */
-  // Save element and its bounds
-  // appUtils.saveEl('[data-element=Rectangle] > svg');`
+  // Save widget and its bounds
+  // appUtils.saveEl('[data-widget=Rectangle] > svg');`
   //   Save `this.el` and `this.elBounds`
-  // appUtils.saveEl('[data-element=Rectangle] > svg', {key: 'svgEl'});`
+  // appUtils.saveEl('[data-widget=Rectangle] > svg', {key: 'svgEl'});`
   //   Save `this.svgEl` and `this.svgElBounds`
   /*
-  before(function dragRectangleElement () {
+  before(function dragRectangleWidget () {
     var svgStartBounds = this.svgElBounds;
     appUtils.click(this.svgEl, {x: 100, y: 200, offsetByBounds: svgStartBounds});
   });
@@ -46,7 +46,7 @@ describe('A drag in the element palette', function () {
 
   // TODO: For dragging...
   /*
-  before(function dragRectangleElement (done) {
+  before(function dragRectangleWidget (done) {
     var svgStartBounds = this.svgElBounds;
     appUtils.drag(this.svgEl, {
       start: {x: 5, y: 5},
@@ -59,17 +59,17 @@ describe('A drag in the element palette', function () {
 
   appUtils.init();
   before(function saveRectanglePosition () {
-    this.svgEl = this.container.querySelector('[data-element=Rectangle] > svg');
+    this.svgEl = this.container.querySelector('[data-widget=Rectangle] > svg');
     this.svgStartBounds = this.svgEl.getBoundingClientRect();
   });
-  before(function dragRectangleElement (done) {
-    // Start dragging our element
+  before(function dragRectangleWidget (done) {
+    // Start dragging our HTML element
     var svgLeft = this.svgStartBounds.left;
     var svgTop = this.svgStartBounds.top;
     var svgEl = this.svgEl;
     simulant.fire(svgEl, 'mousedown', {button: 0, clientX: svgLeft + 5, clientY: svgTop + 5});
 
-    // Then drag and release our element
+    // Then drag and release our HTML element
     setTimeout(function dragEl () {
       simulant.fire(svgEl, 'mousemove', {clientX: svgLeft + 305, clientY: svgTop + 205});
     }, 20);
@@ -78,23 +78,23 @@ describe('A drag in the element palette', function () {
       done();
     }, 40);
   });
-  appUtils.capturePage('element-palette-drag.png');
+  appUtils.capturePage('widget-palette-drag.png');
   after(function cleanup () {
     delete this.svgEl;
     delete this.svgStartBounds;
   });
 
-  it('adds an element to the app', function () {
+  it('adds an widget to the app', function () {
     expect(this.app.layers).to.have.length(1);
-    var component = this.app.layers[0];
-    expect(component).to.be.instanceof(Rectangle);
+    var widget = this.app.layers[0];
+    expect(widget).to.be.instanceof(Rectangle);
   });
 
-  it('positions an element at our expected position', function () {
-    // DEV: We don't use `this.svgEl` since we could create a new rectangle on drop
-    var svgEl = this.container.querySelector('#workspace').childNodes[0];
-    var rectEndBounds = svgEl.getBoundingClientRect();
-    expect(rectEndBounds.left - this.svgStartBounds.left).to.equal(300);
-    expect(rectEndBounds.top - this.svgStartBounds.top).to.equal(200);
+  it('positions an widget at our expected position', function () {
+    // DEV: We don't use `this.svgEl` since we could create a new widget on drop
+    var _svgEl = this.container.querySelector('#workspace').childNodes[0];
+    var svgEndBounds = _svgEl.getBoundingClientRect();
+    expect(svgEndBounds.left - this.svgStartBounds.left).to.equal(300);
+    expect(svgEndBounds.top - this.svgStartBounds.top).to.equal(200);
   });
 });
