74c55af0a48f95ba55d7e8b2f97ded204c7d3cfe
Todd Wolfson <todd@twolfson.com>
2016-03-25 17:23:15 -0500

    Completed our edge case

--- test/widget-palette-drag.js
+++ test/widget-palette-drag.js
@@ -36,25 +36,56 @@ describe('A drag on a scrolled workspace', function () {
   appUtils.init();
   htmlUtils.saveEl('[data-widget=Rectangle] > svg', {key: 'svgEl'});
   htmlUtils.saveEl('#workspace', {key: 'workspaceEl'});
-  // Drag a rectangle to the outskirts of our workspace
   before(function dragRectangleWidget (done) {
+    // Save scroll info so we can verify we moved to the workspace edge
+    this.startScrollBounds = {
+      width: this.workspaceEl.scrollWidth,
+      height: this.workspaceEl.scrollHeight
+    };
+    expect(this.startScrollBounds.width).to.not.equal(0);
+    expect(this.startScrollBounds.height).to.not.equal(0);
+
+    // Drag a rectangle to the outskirts of our workspace
     htmlUtils.dragEl(this.svgEl, {
       start: {x: this.svgElBounds.left + 5, y: this.svgElBounds.top + 5},
       end: {
-        x: this.workspaceEl.right - this.svgElBounds.width + 5,
-        y: this.workspaceEl.bottom - this.svgElBounds.height + 5
+        x: this.workspaceElBounds.right - this.svgElBounds.width + 5,
+        y: this.workspaceElBounds.bottom - this.svgElBounds.height + 5
       }
     }, done);
   });
-  appUtils.capturePage('widget-palette-drag-scrolled.png');
+  before(function scrollWorkspace () {
+    // Verify the scroll bounds changed
+    expect(this.workspaceEl.scrollWidth).to.not.equal(this.startScrollBounds.width);
+    expect(this.workspaceEl.scrollHeight).to.not.equal(this.startScrollBounds.height);
+    expect(this.workspaceEl.scrollTop).to.equal(0);
+    expect(this.workspaceEl.scrollLeft).to.equal(0);
 
-  it('positions an widget at our expected position', function (done) {
-    this.timeout(600000);
-    // DEV: We don't use `this.svgEl` since we could create a new widget on drop
+    // Scroll our workspace
+    // DEV: We have asserts since if there's no room to scroll, then they won't change
+    this.workspaceEl.scrollTop = 10;
+    this.workspaceEl.scrollLeft = 10;
+    expect(this.workspaceEl.scrollTop).to.not.equal(0);
+    expect(this.workspaceEl.scrollLeft).to.not.equal(0);
+  });
+  before(function dragRectangleWidget (done) {
+    // Drag a new rectangle widget to our outer bounds
+    htmlUtils.dragEl(this.svgEl, {
+      start: {x: this.svgElBounds.left + 5, y: this.svgElBounds.top + 5},
+      end: {x: this.svgElBounds.left + 305, y: this.svgElBounds.top + 205}
+    }, done);
+  });
+  appUtils.capturePage('widget-palette-drag-scrolled.png');
+  after(function cleanup () {
+    delete this.startScrollBounds;
+  });
+
+  it('positions an widget at our expected position', function () {
+    // DEV: We don't use `this.svgEl` since we create a new widget on drop
     var _svgEl = this.container.querySelector('#workspace').childNodes[0];
     var svgStartBounds = this.svgElBounds;
     var svgEndBounds = _svgEl.getBoundingClientRect();
-    // expect(svgEndBounds.left - svgStartBounds.left).to.equal(300);
-    // expect(svgEndBounds.top - svgStartBounds.top).to.equal(200);
+    expect(svgEndBounds.left - svgStartBounds.left).to.equal(300);
+    expect(svgEndBounds.top - svgStartBounds.top).to.equal(200);
   });
 });
