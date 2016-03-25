39f33b3e473f117c7eefd1d3b9b3ea34f86ca233
Todd Wolfson <todd@twolfson.com>
2016-03-25 02:26:25 -0500

    Caught edge case for widget palette clicking

--- test/widget-palette-click.js
+++ test/widget-palette-click.js
@@ -2,15 +2,20 @@
 var expect = require('chai').expect;
 var simulant = require('simulant');
 var appUtils = require('./utils/application');
+var htmlUtils = require('./utils/html');
 var Rectangle = require('../lib/js/widgets/rectangle');
 
 // Start our tests
 describe('A click in the widget palette', function () {
   appUtils.init();
+  htmlUtils.saveEl('[data-widget=Rectangle] > svg', {key: 'svgEl'});
   before(function clickRectangleWidget () {
-    // Find and click the rectangle's HTML element in the palette
-    var rectEl = this.container.querySelector('[data-widget=Rectangle]');
-    simulant.fire(rectEl, 'click');
+    // DEV: We don't use click due to draggabillie listening to mousedown/mouseup
+    // DEV: We should note that this click could be on the heading as well
+    var svgLeft = this.svgElBounds.left;
+    var svgTop = this.svgElBounds.top;
+    simulant.fire(this.svgEl, 'mousedown', {button: 0, clientX: svgLeft + 5, clientY: svgTop + 5});
+    simulant.fire(this.svgEl, 'mouseup', {button: 0, clientX: svgLeft + 5, clientY: svgTop + 5});
   });
   appUtils.capturePage('widget-palette-click.png');
 
@@ -21,6 +26,7 @@ describe('A click in the widget palette', function () {
   });
 
   it('positions the widget at our expected position', function () {
+    // DEV: We don't use `this.el` since we generate a new widget on click
     var rectEl = this.container.querySelector('#workspace').childNodes[0];
     expect(rectEl.style.top).to.equal('100px');
     expect(rectEl.style.left).to.equal('100px');
